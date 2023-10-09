dc5cf5b04f0501d4d5d916e6f96b46e49690745d
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2023-10-09 19:17:11 +0300

    initial: initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "rebelot/kanagawa.nvim",
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  {
+    'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'
+  },
+  'neovim/nvim-lspconfig',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+})
+
+require("plugins.lsp")
+require("plugins.cmp")
