58ee5c7d5da6fa0c77e458b465ed7b457015bf0b
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2023-10-16 17:01:52 +0300

    feat: moved from vsnip to luasnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
     }
   },
   {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {} -- this is equalent to setup({}) function
   },
@@ -45,8 +45,8 @@ require("lazy").setup({
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-vsnip",
-  "hrsh7th/vim-vsnip"
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip"
 })
 
 require("plugins.p-treesitter")
