99142038088d936c8ed9101599f074531a07eace
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2023-10-16 22:00:59 +0300

    feat: restored vsnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,8 +45,8 @@ require("lazy").setup({
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/nvim-cmp",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip"
+  "hrsh7th/cmp-vsnip",
+  "hrsh7th/vim-vsnip"
 })
 
 require("plugins.p-treesitter")
